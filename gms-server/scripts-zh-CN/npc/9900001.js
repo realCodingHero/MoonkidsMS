d9ed08c661f62e98f9fc6ecc1b49952b4063726d
ellinforest <translival@LIVE.COM>
2026-08-25 17:06:00 +0900

    feat(script): streamline Maple Helper 9900001 menus for players and GMs

--- gms-server/scripts-zh-CN/npc/9900001.js
+++ gms-server/scripts-zh-CN/npc/9900001.js
@@ -21,13 +21,16 @@
 */
 
 /**
- * @description 拍卖行中心脚本
+ * @description 枫叶助手 / 脚本中心
  */
-var OldTitle ="\t\t\t\t\t#e欢迎来到#rBeiDou#k脚本中心#n\t\t\t\t\r\n";
+var OldTitle = "\t\t\t\t\t#e欢迎来到#rBeiDou#k枫叶助手#n\t\t\t\t\r\n";
 var status = -1;
-var i = 0;
+var currentMenu = 0; // 0: 主菜单, 1: 更多... (玩家), 2: 更多 (GM)
+
 function start() {
-    action(1, 0, 0)
+    status = -1;
+    currentMenu = 0;
+    action(1, 0, 0);
 }
 
 function action(mode, type, selection) {
@@ -41,27 +44,53 @@ function action(mode, type, selection) {
     }
 
     if (status === 0) {
-		let text = OldTitle;
+        currentMenu = 0;
+        let text = OldTitle;
         text += "当前点券：" + cm.getPlayer().getCashShop().getCash(1) + "\r\n";
         text += "当前抵用券：" + cm.getPlayer().getCashShop().getCash(2) + "\r\n";
         text += "当前信用券：" + cm.getPlayer().getCashShop().getCash(4) + "\r\n";
-		text += "当前金币：" + cm.getPlayer().getMeso() + "\r\n";
+        text += "当前金币：" + cm.getPlayer().getMeso() + "\r\n";
         text += " \r\n\r\n";
-		text += "#L3#传送自由#l \t #L69#快速转职#l \t #L70#学习技能#l\r\n";
-		text += "#L71#超级传送#l \t #L4#爆率一览#l \t #L2#在线奖励#l\r\n";
-        text += "#L0#新人福利#l \t #L1#每日签到#l  \t #L72#转世重生#l\r\n";
-        text += "#L80#任务辅助#l \t #L81#装备商店#l \t #L82#宠物杂物设置#l\r\n";
-		// text += "#L999#测试脚本>>>未上线#l \t \r\n";
+        text += "#b#L71#超级传送#l \t #L80#任务辅助#l\r\n";
+        text += "#L81#装备商店#l \t #L82#宠物杂物设置#l\r\n";
+        text += "#L100#更多...#l#k\r\n";
+
         if (cm.getPlayer().isGM()) {
-            text += "\r\n\r\n";
+            text += "\r\n";
             text += "\t\t\t\t#r=====以下内容仅GM可见=====\r\n";
-            text += "#L61#超级传送#l \t #L62#超级商店#l \t #L63#整容集合#l\r\n\r\n";
-			text += "#L64#UI查询#l \t #L65#一键删除道具#l \t #L66#一键刷道具#l\r\n\r\n";
-			text += "#L67#有状态脚本示例#l \t #L68#NextLevel脚本示例#l";
+            text += "#L62#超级商店#l \t #L63#整容集合#l\r\n";
+            text += "#L200#更多 (GM)#l\r\n";
         }
         cm.sendSimple(text);
     } else if (status === 1) {
-        doSelect(selection);
+        if (selection === 100) {
+            // 普通玩家：更多...
+            currentMenu = 1;
+            let text = "\t\t\t\t\t#e【 枫叶助手 - 更多功能 】#n\r\n\r\n";
+            text += "#b#L3#传送自由#l \t #L69#快速转职#l \t #L70#学习技能#l\r\n";
+            text += "#L72#转世重生#l \t #L4#爆率一览#l \t #L2#在线奖励#l\r\n";
+            text += "#L0#新人福利#l \t #L1#每日签到#l\r\n\r\n";
+            text += "#L9999##b[返回主菜单]#k#l";
+            cm.sendSimple(text);
+        } else if (selection === 200 && cm.getPlayer().isGM()) {
+            // GM：更多 (GM)
+            currentMenu = 2;
+            let text = "\t\t\t\t\t#r#e【 GM 管理与调试功能 】#n#k\r\n\r\n";
+            text += "#b#L64#UI查询#l \t #L65#一键删除道具#l \t #L66#一键刷道具#l\r\n\r\n";
+            text += "#L67#有状态脚本示例#l \t #L68#NextLevel脚本示例#l\r\n\r\n";
+            text += "#L9999##b[返回主菜单]#k#l";
+            cm.sendSimple(text);
+        } else if (selection === 9999) {
+            start();
+        } else {
+            doSelect(selection);
+        }
+    } else if (status === 2) {
+        if (selection === 9999) {
+            start();
+        } else {
+            doSelect(selection);
+        }
     } else {
         cm.dispose();
     }
@@ -70,6 +99,9 @@ function action(mode, type, selection) {
 function doSelect(selection) {
     switch (selection) {
         // 非GM功能
+        case 71:
+            openNpc("万能传送");
+            break;
         case 80:
             openNpc("questHelper");
             break;
@@ -79,21 +111,25 @@ function doSelect(selection) {
         case 82:
             openNpc("petLootSetting");
             break;
-		case 999:
-            openNpc("测试脚本");
+        case 3:
+            cm.getPlayer().saveLocation("FREE_MARKET");
+            cm.warp(910000000, "out00");
+            cm.dispose();
             break;
-
         case 69:
             openNpc("快速转职");
             break;
         case 70:
             openNpc("技能学习");
             break;
-        case 71:
-            openNpc("万能传送");
-            break;
         case 72:
             openNpc("转世重生");
+            break;
+        case 4:
+            openNpc("当前地图掉落");
+            break;
+        case 2:
+            openNpc("在线奖励_nextlevel");
             break;
         case 0:
             openNpc("新人福利");
@@ -101,20 +137,8 @@ function doSelect(selection) {
         case 1:
             openNpc("每日签到");
             break;
-        case 2:
-            openNpc("在线奖励_nextlevel");
-            break;
-        case 3:
-            cm.getPlayer().saveLocation("FREE_MARKET");
-            cm.warp(910000000, "out00");
-            break;
-        case 4:
-            openNpc("当前地图掉落");
-            break;
+
         // GM功能
-        case 61:
-            openNpc("万能传送");
-            break;
         case 62:
             cm.dispose();
             cm.openShopNPC(9900001);
@@ -125,7 +149,7 @@ function doSelect(selection) {
             break;
         case 64:
             openNpc("UI查询");
-            break;	
+            break;
         case 65:
             openNpc("一键删除道具");
             break;
@@ -133,12 +157,11 @@ function doSelect(selection) {
             openNpc("一键刷道具");
             break;
         case 67:
-            openNpc("Example1")
+            openNpc("Example1");
             break;
         case 68:
-            openNpc("Example2")
+            openNpc("Example2");
             break;
-
 
         default:
             cm.sendOk("该功能暂不支持，敬请期待！");
