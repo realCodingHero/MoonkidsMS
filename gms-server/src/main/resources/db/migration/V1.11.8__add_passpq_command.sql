5e709f2e739e3da323191e5c9bf8361f13131e49
ellinforest <translival@LIVE.COM>
2026-09-02 05:38:13 +0900

    fix(command): add V1.11.8 migration to register passpq in command_info table

---
+++ gms-server/src/main/resources/db/migration/V1.11.8__add_passpq_command.sql
@@ -0,0 +1,2 @@
+INSERT INTO command_info (syntax, level, enabled, clazz, default_level) VALUES ('passpq', 2, 1, 'PassPqCommand', 2);
+INSERT INTO command_info (syntax, level, enabled, clazz, default_level) VALUES ('clearpq', 2, 1, 'PassPqCommand', 2);
